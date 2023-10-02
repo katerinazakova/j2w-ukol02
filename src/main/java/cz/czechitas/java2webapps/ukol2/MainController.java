610a6cd9a7f381aa3258722231a06bfd6df3535a
7ZKaty <zakova.kz@gmail.com>
2023-10-02 12:54:04 +0200

    J2-ukol02: setting quotes variability.

--- src/main/java/cz/czechitas/java2webapps/ukol2/MainController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol2/MainController.java
@@ -4,18 +4,39 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 @Controller
 public class MainController {
     private final Random random = new Random();
 
     @GetMapping("/")
-    public ModelAndView changingQuoteAndPhoto() {
+    public ModelAndView settingChangingQuoteAndPhoto() throws IOException {
         ModelAndView result = new ModelAndView("index");
-
-        int randomNumber = random.nextInt(7) + 1;
+        int randomNumber = random.nextInt(6);
         result.addObject("fotografie", String.format("/images/fotografie-%d.jpg", randomNumber));
+
+        List<String> allQuotes = readAllLines("R. Fulghum - citáty.txt");
+        String quote = allQuotes.get(randomNumber);
+        result.addObject("quote", quote);
+
         return result;
+    }
+
+    private static List<String> readAllLines(String resources) throws IOException {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        try (InputStream inputStream = classLoader.getResourceAsStream(resources);
+             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+            return reader
+                    .lines()
+                    .collect(Collectors.toList());
+        }
     }
 }
